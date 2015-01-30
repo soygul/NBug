3f6f26c39e6b0752b39c62bfb166a51b25fc6530
schoetbi <schoetbi@schaeffler.com>
2015-01-30 15:15:02 +0100

    Modified Dispatcher to static class that manages a thread that runs till all pending exceptions have been sent BugReport restarts the Dispatcher to that the new Bugreport will be sent and then wait for completion before exit.

--- NBug/Core/Submission/Web/Mail.cs
+++ NBug/Core/Submission/Web/Mail.cs
@@ -6,192 +6,215 @@
 
 namespace NBug.Core.Submission.Web
 {
-	using System;
-	using System.IO;
-	using System.Net;
-	using System.Net.Mail;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq.Expressions;
+    using System.Net;
+    using System.Net.Mail;
 
-	using NBug.Core.Reporting.Info;
-	using NBug.Core.Util.Logging;
-	using NBug.Core.Util.Serialization;
+    using NBug.Core.Reporting.Info;
+    using NBug.Core.Util.Logging;
+    using NBug.Core.Util.Serialization;
 
-	public class MailFactory : IProtocolFactory
-	{
-		public string SupportedType
-		{
-			get
-			{
-				return "Mail";
-			}
-		}
+    public class MailFactory : IProtocolFactory
+    {
+        public string SupportedType
+        {
+            get
+            {
+                return "Mail";
+            }
+        }
 
-		public IProtocol FromConnectionString(string connectionString)
-		{
-			return new Mail(connectionString);
-		}
-	}
+        public IProtocol FromConnectionString(string connectionString)
+        {
+            return new Mail(connectionString);
+        }
+    }
 
-	public class Mail : ProtocolBase
-	{
-		public Mail(string connectionString)
-			: base(connectionString)
-		{
-		}
+    public class Mail : ProtocolBase
+    {
+        public Mail(string connectionString)
+            : base(connectionString)
+        {
+        }
 
-		public Mail()
-		{
-			this.Port = 25;
-		}
+        public Mail()
+        {
+            this.Port = 25;
+        }
 
-		// Connection string format (single line)
-		// Warning: There should be no semicolon (;) or equals sign (=) used in any field except for password.
-		// Warning: Password filed should be the last item.
-		// Warning: No fild value value should contain the phrase 'password='
-		// Note: Normal ports may be 25/26 depending on the host!
-		// Note: Priority can be High, Normal, Low (Normal by default).
+        // Connection string format (single line)
+        // Warning: There should be no semicolon (;) or equals sign (=) used in any field except for password.
+        // Warning: Password filed should be the last item.
+        // Warning: No fild value value should contain the phrase 'password='
+        // Note: Normal ports may be 25/26 depending on the host!
+        // Note: Priority can be High, Normal, Low (Normal by default).
 
-		/* Type=Mail;
-		 * From=my_tracker@gmail.com;
-		 * FromName=NBug Error Reporter;
-		 * To=bugtracker@mycompany.com,someone@dummy.com,my_tracker@gmail.com;
-		 * Cc=;
-		 * Bcc=;
-		 * ReplyTo=;
-		 * UseAttachment=false;
-		 * CustomSubject=;
-		 * CustomBody=;
-		 * SmtpServer=smtp.gmail.com;
-		 * UseSSL=yes;
-		 * Port=465;
-		 * Priority=;
-		 * UseAuthentication=yes;
-		 * Username=my_tracker@gmail.com;
-		 * Password=mypassword;
-		 */
-		public string Bcc { get; set; }
+        /* Type=Mail;
+         * From=my_tracker@gmail.com;
+         * FromName=NBug Error Reporter;
+         * To=bugtracker@mycompany.com,someone@dummy.com,my_tracker@gmail.com;
+         * Cc=;
+         * Bcc=;
+         * ReplyTo=;
+         * UseAttachment=false;
+         * CustomSubject=;
+         * CustomBody=;
+         * SmtpServer=smtp.gmail.com;
+         * UseSSL=yes;
+         * Port=465;
+         * Priority=;
+         * UseAuthentication=yes;
+         * Username=my_tracker@gmail.com;
+         * Password=mypassword;
+         */
+        public string Bcc { get; set; }
 
-		public string Cc { get; set; }
+        public string Cc { get; set; }
 
-		public string CustomBody { get; set; }
+        public string CustomBody { get; set; }
 
-		public string CustomSubject { get; set; }
+        public string CustomSubject { get; set; }
 
-		public string From { get; set; }
+        public string From { get; set; }
 
-		public string FromName { get; set; }
+        public string FromName { get; set; }
 
-		public string Password { get; set; }
+        public string Password { get; set; }
 
-		public int Port { get; set; }
+        public int Port { get; set; }
 
-		public MailPriority Priority { get; set; }
+        public MailPriority Priority { get; set; }
 
-		public string ReplyTo { get; set; }
+        public string ReplyTo { get; set; }
 
-		public string SmtpServer { get; set; }
+        public string SmtpServer { get; set; }
 
-		public string To { get; set; }
+        public string To { get; set; }
 
-		public bool UseAttachment { get; set; }
+        public bool UseAttachment { get; set; }
 
-		public bool UseAuthentication { get; set; }
+        public bool UseAuthentication { get; set; }
 
-		public bool UseSsl { get; set; }
+        public bool UseSsl { get; set; }
 
-		public string Username { get; set; }
+        public string Username { get; set; }
 
-		public override bool Send(string fileName, Stream file, Report report, SerializableException exception)
-		{
-			if (string.IsNullOrEmpty(this.From) || string.IsNullOrEmpty(this.To))
-			{
-				return false;
-			}
+        public override bool Send(string fileName, Stream file, Report report, SerializableException exception)
+        {
+            if (string.IsNullOrEmpty(this.From) || string.IsNullOrEmpty(this.To))
+            {
+                return false;
+            }
 
-			if (string.IsNullOrEmpty(this.ReplyTo))
-			{
-				this.ReplyTo = this.From;
-			}
+            if (string.IsNullOrEmpty(this.ReplyTo))
+            {
+                this.ReplyTo = this.From;
+            }
 
-			if (this.Port <= 0)
-			{
-				this.Port = this.UseSsl ? 465 : 25;
-			}
+            if (this.Port <= 0)
+            {
+                this.Port = this.UseSsl ? 465 : 25;
+            }
 
-			if (!this.UseAttachment)
-			{
-				this.UseAttachment = false;
-			}
+            if (!this.UseAttachment)
+            {
+                this.UseAttachment = false;
+            }
 
-			// Make sure that we can use authentication even with emtpy username and password
-			if (!string.IsNullOrEmpty(this.Username))
-			{
-				this.UseAuthentication = true;
-			}
+            // Make sure that we can use authentication even with emtpy username and password
+            if (!string.IsNullOrEmpty(this.Username))
+            {
+                this.UseAuthentication = true;
+            }
 
-			using (var smtpClient = new SmtpClient())
-			using (var message = new MailMessage())
-			{
-				if (!string.IsNullOrEmpty(this.SmtpServer))
-				{
-					smtpClient.Host = this.SmtpServer;
-				}
+            SmtpClient smtpClient = null;
+            try
+            {
+                smtpClient = new SmtpClient();
 
-				smtpClient.Port = this.Port;
+                using (var message = new MailMessage())
+                {
+                    if (!string.IsNullOrEmpty(this.SmtpServer))
+                    {
+                        smtpClient.Host = this.SmtpServer;
+                    }
 
-				if (this.UseAuthentication)
-				{
-					smtpClient.Credentials = new NetworkCredential(this.Username, this.Password);
-				}
+                    smtpClient.Port = this.Port;
 
-				smtpClient.EnableSsl = this.UseSsl;
+                    if (this.UseAuthentication)
+                    {
+                        smtpClient.Credentials = new NetworkCredential(this.Username, this.Password);
+                    }
 
-				if (!string.IsNullOrEmpty(this.Cc))
-				{
-					message.CC.Add(this.Cc);
-				}
+                    smtpClient.EnableSsl = this.UseSsl;
 
-				if (!string.IsNullOrEmpty(this.Bcc))
-				{
-					message.Bcc.Add(this.Bcc);
-				}
+                    if (!string.IsNullOrEmpty(this.Cc))
+                    {
+                        message.CC.Add(this.Cc);
+                    }
 
-				message.Priority = this.Priority;
+                    if (!string.IsNullOrEmpty(this.Bcc))
+                    {
+                        message.Bcc.Add(this.Bcc);
+                    }
 
-				message.To.Add(this.To);
-				message.ReplyToList.Add(this.ReplyTo);
-				message.From = !string.IsNullOrEmpty(this.FromName) ? new MailAddress(this.From, this.FromName) : new MailAddress(this.From);
+                    message.Priority = this.Priority;
 
-				if (this.UseAttachment)
-				{
-					// ToDo: Report file name should be attached to the report file object itself, file shouldn't be accessed directly!
-					file.Position = 0;
-					message.Attachments.Add(new Attachment(file, fileName));
-				}
+                    message.To.Add(this.To);
+                    message.ReplyToList.Add(this.ReplyTo);
+                    message.From = !string.IsNullOrEmpty(this.FromName)
+                                       ? new MailAddress(this.From, this.FromName)
+                                       : new MailAddress(this.From);
 
-				if (!string.IsNullOrEmpty(this.CustomSubject))
-				{
-					message.Subject = this.CustomSubject;
-				}
-				else
-				{
-					message.Subject = "NBug: " + report.GeneralInfo.HostApplication + " (" + report.GeneralInfo.HostApplicationVersion + "): "
-					                  + report.GeneralInfo.ExceptionType + " @ " + report.GeneralInfo.TargetSite;
-				}
+                    if (this.UseAttachment)
+                    {
+                        // ToDo: Report file name should be attached to the report file object itself, file shouldn't be accessed directly!
+                        file.Position = 0;
+                        message.Attachments.Add(new Attachment(file, fileName));
+                    }
 
-				if (!string.IsNullOrEmpty(this.CustomBody))
-				{
-					message.Body = this.CustomBody + Environment.NewLine + Environment.NewLine + report + Environment.NewLine + Environment.NewLine + exception;
-				}
-				else
-				{
-					message.Body = report + Environment.NewLine + Environment.NewLine + exception;
-				}
+                    if (!string.IsNullOrEmpty(this.CustomSubject))
+                    {
+                        message.Subject = this.CustomSubject;
+                    }
+                    else
+                    {
+                        message.Subject = "NBug: " + report.GeneralInfo.HostApplication + " ("
+                                          + report.GeneralInfo.HostApplicationVersion + "): "
+                                          + report.GeneralInfo.ExceptionType + " @ " + report.GeneralInfo.TargetSite;
+                    }
 
-				smtpClient.Send(message);
-				Logger.Trace("Submitted bug report email to: " + this.To);
+                    if (!string.IsNullOrEmpty(this.CustomBody))
+                    {
+                        message.Body = this.CustomBody + Environment.NewLine + Environment.NewLine + report
+                                       + Environment.NewLine + Environment.NewLine + exception;
+                    }
+                    else
+                    {
+                        message.Body = report + Environment.NewLine + Environment.NewLine + exception;
+                    }
 
-				return true;
-			}
-		}
-	}
+                    smtpClient.Send(message);
+                    Logger.Trace("Submitted bug report email to: " + this.To);
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Trace(ex.ToString());
+            }
+            finally
+            {
+                if (smtpClient != null)
+                {
+                    smtpClient.Dispose();
+                }
+            }
+            return false;
+        }
+    }
 }
