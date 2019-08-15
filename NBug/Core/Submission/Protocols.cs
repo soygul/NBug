b1aa9122404efa55f7304b867a601494c42d8a30
Teoman Soygul <soygul@users.noreply.github.com>
2019-08-15 18:51:30 +0200

    fix protocols.cs license header

--- NBug/Core/Submission/Protocols.cs
+++ NBug/Core/Submission/Protocols.cs
@@ -1,6 +1,6 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Protocols.cs" company="NBusy Project">
-//   Copyright (c) 2010 - 2011 Teoman Soygul. Licensed under LGPLv3 (http://www.gnu.org/licenses/lgpl.html).
+//   Copyright (c) 2010 - 2011 Teoman Soygul. Licensed under MIT license.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
